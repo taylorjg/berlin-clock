95bc4df60b4088caeea10e3fc4bab8bc84b87d19
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-06-14 14:12:30 +0100

    Make solution less verbose

--- src/index.js
+++ src/index.js
@@ -1,42 +1,18 @@
 const moment = require('moment')
 
+const positiveIntegers = n => Array(n).fill(0).map((_, index) => index + 1)
+
 const toBerlinClock = time => {
   const hms = moment.duration(time)
   const h = hms.hours()
   const m = hms.minutes()
   const s = hms.seconds()
   return {
-    seconds: s % 2,
-    fiveHours: [
-      h >= 5 ? 1 : 0,
-      h >= 10 ? 1 : 0,
-      h >= 15 ? 1 : 0,
-      h >= 20 ? 1 : 0],
-    oneHours: [
-      h % 5 >= 1 ? 1 : 0,
-      h % 5 >= 2 ? 1 : 0,
-      h % 5 >= 3 ? 1 : 0,
-      h % 5 >= 4 ? 1 : 0
-    ],
-    fiveMinutes: [
-      m >= 5 ? 1 : 0,
-      m >= 10 ? 1 : 0,
-      m >= 15 ? 1 : 0,
-      m >= 20 ? 1 : 0,
-      m >= 25 ? 1 : 0,
-      m >= 30 ? 1 : 0,
-      m >= 35 ? 1 : 0,
-      m >= 40 ? 1 : 0,
-      m >= 45 ? 1 : 0,
-      m >= 50 ? 1 : 0,
-      m >= 55 ? 1 : 0
-    ],
-    oneMinutes: [
-      m % 5 >= 1 ? 1 : 0,
-      m % 5 >= 2 ? 1 : 0,
-      m % 5 >= 3 ? 1 : 0,
-      m % 5 >= 4 ? 1 : 0
-    ]
+    seconds: Number(s % 2),
+    fiveHours: positiveIntegers(4).map(n => Number(h >= n * 5)),
+    oneHours: positiveIntegers(4).map(n => Number(h % 5 >= n)),
+    fiveMinutes: positiveIntegers(11).map(n => Number(m >= n * 5)),
+    oneMinutes: positiveIntegers(4).map(n => Number(m % 5 >= n))
   }
 }
 
